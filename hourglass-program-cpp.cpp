68011e05418d8debe2f0cda32e9816f3c6ff15b2
Vaishnavi Verma <vaiatsunrise@gmail.com>
2025-07-27 22:14:41 +0530

    Add files via upload

---
+++ hourglass-program-cpp.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+
+    cout << "\nHourglass Pattern:\n";
+    for (int i = n; i >= 1; i--) {
+        for (int j = 1; j <= n - i; j++)
+            cout << "  ";
+        for (int k = 1; k <= 2 * i - 1; k++)
+            cout << "* ";
+        cout << endl;
+    }
+    for (int i = 2; i <= n; i++) {
+        for (int j = 1; j <= n - i; j++)
+            cout << "  ";
+        for (int k = 1; k <= 2 * i - 1; k++)
+            cout << "* ";
+        cout << endl;
+    }
+    return 0;
+}
